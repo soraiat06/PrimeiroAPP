128b6032e0c3e1bab766c42107ec5f061eeded76
20221TIIMI0270 <soraiatosdeoliveira@hotmail.com>
2024-04-05 10:26:10 -0300

    Inicio dos comentarios em MainActivity.java

--- app/src/main/java/tavares/soraia/primeiroapp/MainActivity.java
+++ app/src/main/java/tavares/soraia/primeiroapp/MainActivity.java
@@ -8,23 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity { // A classe MainActivity e a que abre o app
+public class MainActivity extends AppCompatActivity { // Declaração da classe MainActivity que estende AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { // Método onCreate que é chamado quando a atividade é criada
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Constroi os elementos de interfase
-        Button btnEnviar=findViewById(R.id.btnEnviar); // Referencia o botão
-        btnEnviar.setOnClickListener(new View.OnClickListener() { // Configura um listener de classe para o botão
+        setContentView(R.layout.activity_main); // Constroi elementos de interface definidos o arquivo xml
+
+        Button btnEnviar = findViewById(R.id.btnEnviar); // Referencia o botão
+        btnEnviar.setOnClickListener(new View.OnClickListener() { // Configura um listener de clique para o botão
             @Override
             public void onClick(View v) { // Método chamado quando o botão é clicado
 
-                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui); // referencia o campo de texto pelo id etDigiteAqui
-                String textoDigitado = etDigiteAqui.getText().toString(); // Obtemmo texto digitado como dado extra no intent
-                Intent i= new Intent(MainActivity.this,NextActivity.class);
-                i.putExtra("texto",textoDigitado); // adiciona o texto digitado
-                startActivity(i);
+                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui); // Referencia o campo de texto pelo ID etDigiteAqui
+                String textoDigitado = etDigiteAqui.getText().toString(); // Obtém o texto digitado no campo de texto
 
+                Intent i = new Intent(MainActivity.this, NextActivity.class); // Cria um novo objeto Intent para iniciar NextActivity
+                i.putExtra("texto", textoDigitado); // Adiciona o texto digitado como dado extra no Intent
+                startActivity(i); // Inicia NextActivity com o Intent configurado
             }
         });
     }
--- app/src/main/java/tavares/soraia/primeiroapp/NextActivity.java
+++ app/src/main/java/tavares/soraia/primeiroapp/NextActivity.java
@@ -1,21 +1,20 @@
 package tavares.soraia.primeiroapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class NextActivity extends AppCompatActivity {
+public class NextActivity extends AppCompatActivity { // Declaração da classe NextActivity que estende AppCompatActivity
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //Método onCreate que é chamado quando a atividade é criada
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-        Intent i = getIntent();
-        String textoDigitado = i.getStringExtra("texto");
-        TextView tvtexto = findViewById(R.id.tvTexto);
-        tvtexto.setText(textoDigitado);
+
+        Intent i = getIntent(); // Obtém o Intent do começo
+        String textoDigitado = i.getStringExtra("texto"); //Obtém o texto extra passado pelo Intent
+        TextView tvTexto = findViewById(R.id.tvTexto);
+        tvTexto.setText(textoDigitado);
     }
 }
-
